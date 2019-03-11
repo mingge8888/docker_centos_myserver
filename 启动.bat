1eb02b9dd827c885f60dd2f211f1f28dfd655d87
mingge <399195513@qq.com>
2019-03-11 19:59:19 +0800

    1

--- 启动.bat
+++ 启动.bat
@@ -1,30 +1,35 @@
-@ECHO OFF&PUSHD %~DP0 &TITLE docker容器启动工具,开发作者:岑明 QQ2945157617
+node-v10.15.3-linux-x64/bin
+@ECHO OFF
+@ECHO OFF&PUSHD %~DP0 &TITLE docker容器启动工具开发 作者:岑明 QQ2945157617
 :main
-MODE con: COLS=70 lines=40
+MODE con: COLS=70 lines=59
 cls
-color 1f      
-:start
+color 1f
+set INTERVAL=10
+:Again
 docker ps>nul 2>nul
- if "%errorlevel%"=="0"  (
+if "%errorlevel%"=="1" (
+color 1c
+echo       ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+echo       ┃                                                      ┃
+echo       ┃             docker服务未启动,正在重试...             ┃
+echo       ┃                                                      ┃
+echo       ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+ 
+timeout %INTERVAL%>nul
+ 
+goto Again
+)  else (
 color 1f
 echo       ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 echo       ┃                                                      ┃
 echo       ┃              docker服务已启动,正在启动容器...        ┃
 echo       ┃                                                      ┃
 echo       ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-docker-compose kill  
-docker-compose up  -d
- )  else (
-color 1c
-echo       ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-echo       ┃                                                      ┃
-echo       ┃             docker服务未启动,正在重试...             ┃
-echo       ┃                                                      ┃
-echo       ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
-choice /t 4 /d y /n >nul
-goto start
+ 
+docker-compose kill
+docker-compose up
  )
- echo                    执行完成！该窗口2秒后自动关闭
-choice /t 2 /d y /n >nul 
-exit  
+cmd /K
+
  
